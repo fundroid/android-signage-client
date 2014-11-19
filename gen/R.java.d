aa4d28d9f1f41ad8bd07a1bd6295883c65847b71
Rui Carmo <rui.carmo@gmail.com>
2014-11-19 11:57:32 +0000

    Reinstated strings.xml.dist

--- gen/R.java.d
+++ gen/R.java.d
@@ -1,18 +1,14 @@
-/home/rcarmo/Development/android-signage-client/gen/eu/codebits/plasmas/R.java \
- : /home/rcarmo/Development/android-signage-client/res/values-v14/styles.xml \
-/home/rcarmo/Development/android-signage-client/res/values-v11/styles.xml \
-/home/rcarmo/Development/android-signage-client/res/drawable-xxhdpi/ic_launcher.png \
-/home/rcarmo/Development/android-signage-client/res/drawable-mdpi/ic_launcher.png \
-/home/rcarmo/Development/android-signage-client/res/layout/activity_fullscreen.xml \
-/home/rcarmo/Development/android-signage-client/res/drawable-hdpi/ic_launcher.png \
-/home/rcarmo/Development/android-signage-client/res/drawable-xhdpi/ic_launcher.png \
-/home/rcarmo/Development/android-signage-client/res/values/attrs.xml \
-/home/rcarmo/Development/android-signage-client/res/values/styles.xml \
-/home/rcarmo/Development/android-signage-client/res/values/strings.xml \
-/home/rcarmo/Development/android-signage-client/res/values/integers.xml \
-/home/rcarmo/Development/android-signage-client/res/values/colors.xml \
-/home/rcarmo/Development/android-signage-client/bin/res/drawable-xxhdpi/ic_launcher.png \
-/home/rcarmo/Development/android-signage-client/bin/res/drawable-mdpi/ic_launcher.png \
-/home/rcarmo/Development/android-signage-client/bin/res/drawable-hdpi/ic_launcher.png \
-/home/rcarmo/Development/android-signage-client/bin/res/drawable-xhdpi/ic_launcher.png \
-/home/rcarmo/Development/android-signage-client/bin/AndroidManifest.xml \
+/Users/rcarmo/Development/android-signage-client/gen/eu/codebits/plasmas/R.java \
+ : /Users/rcarmo/Development/android-signage-client/res/drawable-hdpi/ic_launcher.png \
+/Users/rcarmo/Development/android-signage-client/res/drawable-mdpi/ic_launcher.png \
+/Users/rcarmo/Development/android-signage-client/res/drawable-xhdpi/ic_launcher.png \
+/Users/rcarmo/Development/android-signage-client/res/drawable-xxhdpi/ic_launcher.png \
+/Users/rcarmo/Development/android-signage-client/res/layout/activity_fullscreen.xml \
+/Users/rcarmo/Development/android-signage-client/res/values/attrs.xml \
+/Users/rcarmo/Development/android-signage-client/res/values/colors.xml \
+/Users/rcarmo/Development/android-signage-client/res/values/integers.xml \
+/Users/rcarmo/Development/android-signage-client/res/values/strings.xml \
+/Users/rcarmo/Development/android-signage-client/res/values/styles.xml \
+/Users/rcarmo/Development/android-signage-client/res/values-v11/styles.xml \
+/Users/rcarmo/Development/android-signage-client/res/values-v14/styles.xml \
+/Users/rcarmo/Development/android-signage-client/bin/AndroidManifest.xml \
