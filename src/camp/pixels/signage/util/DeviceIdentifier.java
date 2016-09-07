99e80107cfa8c15c8b4caf795b4620e455e312ea
Rui Carmo <rui.carmo@gmail.com>
2016-09-08 00:04:59 +0100

    Device identifier helpers

---
+++ src/camp/pixels/signage/util/DeviceIdentifier.java
@@ -0,0 +1,50 @@
+
+package camp.pixels.signage.util;
+
+import android.provider.Settings;
+import android.content.Context;
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+
+public class DeviceIdentifier {
+    private static String sID = null;
+    private static final String INSTALLATION = "INSTALLATION";
+
+    public static String hardwareId() {
+        return Settings.Secure.ANDROID_ID;
+    }
+
+    public synchronized static String installationID(Context context) {
+        if (sID == null) {  
+            File installation = new File(context.getFilesDir(), INSTALLATION);
+            try {
+                if (!installation.exists())
+                    writeInstallationFile(installation);
+                sID = readInstallationFile(installation);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return sID;
+    }
+
+
+    private static String readInstallationFile(File installation) throws IOException {
+        RandomAccessFile f = new RandomAccessFile(installation, "r");
+        byte[] bytes = new byte[(int) f.length()];
+        f.readFully(bytes);
+        f.close();
+        return new String(bytes);
+    }
+
+    private static void writeInstallationFile(File installation) throws IOException {
+        FileOutputStream out = new FileOutputStream(installation);
+        String id = UUID.randomUUID().toString();
+        out.write(id.getBytes());
+        out.close();
+    }
+}
